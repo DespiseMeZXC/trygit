1840221e105c925bff5a736ea1eb80897774a6c9
DespiseMeZXC <2003lesha2003@mail.ru>
2023-01-21 02:30:53 +0300

    create new func

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ def minus(first: int, second: int) -> int:
     return first + second
 
 
+def printMinusAndPlus(first: int, second: int) -> int:
+    print(minus(first, second) + " " + sum(first, second))
+
+
 print(sum(5, 25))
 print(minus(10, 20))
+printMinusAndPlus(10, 15)
