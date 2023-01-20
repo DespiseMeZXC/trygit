9eef6b01afeffe6e93aa8d66d27c78f219b57e86
DespiseMeZXC <2003lesha2003@mail.ru>
2023-01-21 02:36:54 +0300

    some change

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def printMinusAndPlus(first: int, second: int) -> int:
     print(minus(first, second) + " " + sum(first, second))
 
 
+def printMinusAndPlus(first: int, second: int) -> int:
+    print(minus(first, second) + " " + sum(first, second))
+
+
 print(sum(5, 25))
 print(minus(10, 20))
 printMinusAndPlus(10, 15)
